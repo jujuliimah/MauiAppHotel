d92b0a8a646d3dead000476c0028741548f447e9
anajulia <anajulia_sdelima@hotmail.com>
2026-05-24 19:16:35 -0300

    Add recursos ao app

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -2,8 +2,45 @@ namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
-	public ContratacaoHospedagem()
-	{
-		InitializeComponent();
-	}
+    App PropriedadesApp;
+
+    public ContratacaoHospedagem()
+    {
+        InitializeComponent();
+
+        PropriedadesApp = (App)Application.Current;
+
+        pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
+        dtpck_checkin.MinimumDate = DateTime.Now;
+        dtpck_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+        dtpck_checkout.MinimumDate = dtpck_checkin.Date.Value.AddDays(1);
+        dtpck_checkout.MaximumDate = dtpck_checkin.Date.Value.AddMonths(6);
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new HospedagemContratada());
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked_2(object sender, EventArgs e)
+    {
+
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        DatePicker elemento = sender as DatePicker;
+
+        DateTime data_selecionada_checkin = elemento.Date.Value;
+
+        dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
+        dtpck_checkout.MaximumDate = data_selecionada_checkin.AddMonths(6);
+    }
 }
